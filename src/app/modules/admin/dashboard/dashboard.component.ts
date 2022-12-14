6079b5c20f3611b8cd55f17b135268afce670d4c
Erik Tomelin <erik.tomelin@valid.com>
2022-12-13 22:23:34 -0300

    addiction of services and requests

--- src/app/modules/admin/dashboard/dashboard.component.ts
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
+import { DiffMovimentsPerSleepService } from 'app/service/diff-moviments-per-sleep.service';
+import { HoursOfSleepService } from 'app/service/hours-of-slee.service';
+import { ApexOptions } from "ng-apexcharts";
 
 import {
     ApexAxisChartSeries,
@@ -26,10 +29,34 @@ export type ChartOptions = {
 
 export class DashboardComponent {
     @ViewChild("chart") chart: ChartComponent;
-    public chartOptions: Partial<ChartOptions>;
 
-    constructor() {
+    public chartOptions: Partial<ApexOptions>;
+
+    constructor(
+        private _diffMovimentsPerSleepService: DiffMovimentsPerSleepService,
+        private _hoursOfSleepService: HoursOfSleepService
+    ) {
         this.chartOptions = {
+            chart: {
+                animations: {
+                    speed: 400,
+                    animateGradually: {
+                        enabled: false
+                    }
+                },
+                fontFamily: 'inherit',
+                foreColor: '#fff',
+                width: window.innerWidth < 1000 ? '1000px' : '100%',
+                height: '340px',
+                type: 'area',
+                toolbar: {
+                    show: false
+                },
+                zoom: {
+                    enabled: false
+                }
+            },
+            colors: ['#008FFB', '#FF6A6A', '#33085c'],
             series: [
                 {
                     name: "Session Duration",
@@ -40,13 +67,21 @@ export class DashboardComponent {
                     data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
                 },
                 {
-                    name: "Total Visits",
-                    data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
+                    name: "Quantidade de Sono Ideal",
+                    data: [70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70],
                 }
             ],
-            chart: {
-                height: 350,
-                type: "line"
+            states: {
+                hover: {
+                    filter: {
+                        type: 'darken',
+                        value: 0.75
+                    }
+                }
+            },
+            fill: {
+                opacity: 1,
+                colors: ['#008FFB', '#FF6A6A', '#33085c'],
             },
             dataLabels: {
                 enabled: false
@@ -57,7 +92,7 @@ export class DashboardComponent {
                 dashArray: [0, 8, 5]
             },
             title: {
-                text: "Page Statistics",
+                text: "MorpCubile Dashboard",
                 align: "left"
             },
             legend: {
@@ -68,6 +103,9 @@ export class DashboardComponent {
                         opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] +
                         "</strong>"
                     );
+                },
+                markers: {
+                    fillColors: ['#008FFB', '#FF6A6A', '#33085c'],
                 }
             },
             markers: {
@@ -78,22 +116,40 @@ export class DashboardComponent {
             },
             xaxis: {
                 labels: {
-                    trim: false
+                    trim: false,
+                    style: {
+                        colors: '#CBD5E1',
+                        fontWeight: 'bold'
+                    }
                 },
                 categories: [
-                    "01 Jan",
-                    "02 Jan",
-                    "03 Jan",
-                    "04 Jan",
-                    "05 Jan",
-                    "06 Jan",
-                    "07 Jan",
-                    "08 Jan",
-                    "09 Jan",
-                    "10 Jan",
-                    "11 Jan",
-                    "12 Jan"
-                ]
+                    "01:00 AM",
+                    "02:00 AM",
+                    "03:00 AM",
+                    "04:00 AM",
+                    "05:00 AM",
+                    "06:00 AM",
+                    "07:00 AM",
+                    "08:00 AM",
+                    "09:00 AM",
+                    "10:00 AM",
+                    "11:00 AM",
+                    "12:00 AM"
+                ],
+            },
+            yaxis: {
+                labels: {
+                    style: {
+                        colors: '#8e8da4',
+                    },
+                    offsetX: -10,
+                },
+                axisBorder: {
+                    show: false,
+                },
+                axisTicks: {
+                    show: false
+                }
             },
             tooltip: {
                 y: [
@@ -118,10 +174,26 @@ export class DashboardComponent {
                             }
                         }
                     }
-                ]
+                ],
+                fillSeriesColor: false,
+                followCursor: true,
+                theme: 'dark',
             },
             grid: {
-                borderColor: "#f1f1f1"
+                show: true,
+                borderColor: "#fff",
+                padding: {
+                    top: 10,
+                    bottom: -10,
+                    left: 25,
+                    right: 10
+                },
+                position: 'back',
+                xaxis: {
+                    lines: {
+                        show: true
+                    }
+                }
             }
         };
     }
